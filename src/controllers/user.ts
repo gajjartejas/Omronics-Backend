8cd8bebfdb821517e56b4ab48300a4065076ffb7
Tejas Gajjar <gajjartejas26@gmail.com>
2023-10-09 21:09:18 +0530

    feat: api auth added

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,6 @@
-import prisma from '../libs/prismaClient'
-const bcrypt = require('bcryptjs')
-var jwt = require('jsonwebtoken')
+import prisma from '../libs/prismaClient';
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 /**
  * POST /users
@@ -18,96 +18,94 @@ var jwt = require('jsonwebtoken')
 // }
 // }
 export const createUser = async (req: any, res: any, next: any) => {
-	try {
-		const { data } = req.body
-    const {  email, password } = data
-    let userCount = await prisma.user.count(
-      {
-        where: {
-          email:email
-        }
-      }
-    )
+  try {
+    const { data } = req.body;
+    const { email, password } = data;
+    let userCount = await prisma.user.count({
+      where: {
+        email: email,
+      },
+    });
     if (userCount > 0) {
-      return res.status(409).send('User Already Exist. Please Login')
+      return res.status(409).send('User Already Exist. Please Login');
     }
-    const encryptedPassword = await bcrypt.hash(password, 10)
+    const encryptedPassword = await bcrypt.hash(password, 10);
     const result = await prisma.user.create({
-			data: {...data, password: encryptedPassword},
-		})
-		res.json(result)
-	} catch (err) {
-		return next(err)
-	}
-}
+      data: { ...data, password: encryptedPassword },
+    });
+    res.json(result);
+  } catch (err) {
+    return next(err);
+  }
+};
 
 /**
  * PATCH /users
  * Update single users
  */
 export const updateUser = async (req: any, res: any, next: any) => {
-	try {
-		const { id }: { id?: string } = req.params
-		const { data } = req.body
-		const result = await prisma.user.update({
-			where: {
-				id: Number(id),
-			},
-			data: data,
-		})
-		res.json(result)
-	} catch (err) {
-		return next(err)
-	}
-}
+  try {
+    const { id }: { id?: string } = req.params;
+    const { data } = req.body;
+    const result = await prisma.user.update({
+      where: {
+        id: Number(id),
+      },
+      data: data,
+    });
+    res.json(result);
+  } catch (err) {
+    return next(err);
+  }
+};
 
 /**
  * DELETE /users
  * Delete users
  */
 export const deleteUser = async (req: any, res: any, next: any) => {
-	try {
-		const { id }: { id?: string } = req.params
-		const result = await prisma.user.delete({
-			where: {
-				id: Number(id),
-			},
-		})
-		res.json(result)
-	} catch (err) {
-		return next(err)
-	}
-}
+  try {
+    const { id }: { id?: string } = req.params;
+    const result = await prisma.user.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    res.json(result);
+  } catch (err) {
+    return next(err);
+  }
+};
 
 /**
  * GET /users
  * Get all users
  */
 export const getUsers = async (req: any, res: any, next: any) => {
-	try {
-		const users = await prisma.user.findMany({})
-		res.json(users)
-	} catch (err) {
-		return next(err)
-	}
-}
+  try {
+    const users = await prisma.user.findMany({});
+    res.json(users);
+  } catch (err) {
+    return next(err);
+  }
+};
 
 /**
  * GET /users/:id
  * Get user by id
  */
 export const getUserById = async (req: any, res: any, next: any) => {
-	try {
-		const { id }: { id?: string } = req.params
+  try {
+    const { id }: { id?: string } = req.params;
 
-		const post = await prisma.user.findUnique({
-			where: { id: Number(id) },
-		})
-		res.json(post)
-	} catch (err) {
-		return next(err)
-	}
-}
+    const post = await prisma.user.findUnique({
+      where: { id: Number(id) },
+    });
+    res.json(post);
+  } catch (err) {
+    return next(err);
+  }
+};
 
 /**
  * POST /login
@@ -115,38 +113,38 @@ export const getUserById = async (req: any, res: any, next: any) => {
  */
 export const login = async (req: any, res: any, next: any) => {
   try {
-    const { data } = req.body
-    const {  email, password } = data
+    const { data } = req.body;
+    const { email, password } = data;
 
     // Validate user input
     if (!(email && password)) {
-      return res.status(400).send('All input is required')
+      return res.status(400).send('All input is required');
     }
-    let user = await prisma.user.findFirst(
-      {
-        where: {
-          email:email
-        }
-      }
-    )
+    let user = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    // 3hrs in sec
+    const maxAge = 3 * 60 * 60;
 
     if (user && (await bcrypt.compare(password, user.password))) {
       // Create token
-      const token = jwt.sign(
-        { id: user.id, email },
-        process.env.JWT_KEY,
-        {
-          expiresIn: '2h',
-        }
-      )
+      const token = jwt.sign({ id: user.id, email }, process.env.JWT_KEY, {
+        expiresIn: maxAge,
+      });
+
+      res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: maxAge * 1000, // 3hrs in ms
+      });
 
       // user
-      res.status(200).json({...user, token: token})
+      res.status(200).json({ ...user, token: token });
     }
-    res.status(400).send('Invalid Credentials')
 
-
+    res.status(200).send({ message: 'Unknown error!' });
   } catch (err) {
-    return next(err)
+    return next(err);
   }
-}
+};
--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,35 +1,34 @@
-// var jwt = require('jsonwebtoken')
+var jwt = require('jsonwebtoken');
 
 function authenticateToken(req: any, res: any, next: any) {
-  return next()
-  // console.log('req.method',req.method)
-  // console.log('req.method',req.path)
-  // const nonSecurePaths = ['/users/login']
-  // if (nonSecurePaths.includes(req.path)) {
-  //   return next() 
-  // }
+  //return next();
+  console.log('req.method', req.method);
+  console.log('req.method', req.path);
 
+  const nonSecurePaths = ['/users/login'];
+  if (nonSecurePaths.includes(req.path)) {
+    return next();
+  }
 
-  // if (req.method === 'GET') {
-  //   return next()
-  // }
+  if (req.method === 'GET') {
+    return next();
+  }
 
-  // const authHeader = req.headers['authorization']
-  // const token = authHeader && authHeader.split(' ')[1]
+  const authHeader = req.headers['authorization'];
+  const headerToken = authHeader && authHeader.split(' ')[1];
+  const cookieToken = req.cookies.jwt;
+  const token = headerToken || cookieToken;
 
-  // if (token == null) {
-  //   return res.sendStatus(401)
-  // }
+  if (token == null) {
+    return res.sendStatus(401);
+  }
 
-  // jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
-  //   console.log(err)
-
-  //   if (err) return res.sendStatus(403)
-
-  //   req.user = user
-
-  //   next()
-  // })
+  jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
-export default authenticateToken
+export default authenticateToken;
